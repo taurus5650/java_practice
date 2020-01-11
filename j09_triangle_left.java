8a9b6f7a55dcf204ae327eb32a6ff8276e073ce6
taurus5650 <taurus_5650@hotmail.com>
2020-01-11 23:24:02 +0800

    Add files via upload
    20200112 remark

--- j09_triangle_left.java
+++ j09_triangle_left.java
@@ -2,9 +2,9 @@ public class j09_triangle_left{
 
      public static void main(String []args){
        for(int i=1;i<=5;i++){
-           /i為縱軸，i總共有5行/
+           /*i為縱軸，i總共有5行*/
              for(int j=1;j<=i;j++)
-             /j為輸出*，當j<=i迴圈里就繼續執行再加顆**/
+             /*j為輸出*，當j<=i迴圈里就繼續執行再加顆**/
            System.out.print("*");
            System.out.println();
          
